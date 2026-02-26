d0a43c1b398c61aa31055de8774d30a0fd7b6a19
svenroth19 <sro152871@stud.gibb.ch>
2026-02-26 08:35:20 +0100

    Change DATABASE_URL to specific PostgreSQL connection string
    Updated DATABASE_URL to a specific PostgreSQL connection string.

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,3 @@
 import { neon } from "@neondatabase/serverless";
 
-export const sql = neon(process.env.DATABASE_URL!);
+export const sql = neon(process.env.postgresql://neondb_owner:npg_7Wi8ItrCEYuc@ep-morning-base-almefz05.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require!);
